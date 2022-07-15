f879e346127068a420ad5004e9efda1ef3a4b4aa
AJAY KUMAR GOND <initajay@gmail.com>
2022-07-15 14:03:32 +0530

    Updates

---
+++ precache-manifest.768b9ec5251ed678bb3b20226b5c73d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32174008d4329ada7684",
+    "url": "/static/css/main.9237a284.chunk.css"
+  },
+  {
+    "revision": "32174008d4329ada7684",
+    "url": "/static/js/main.32174008.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "0a3bfdb56ed648ab96e38c91487bf8fa",
+    "url": "/index.html"
+  }
+];
